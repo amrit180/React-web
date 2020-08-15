87db69fdad7694f040ea05f2196c89b8fd986352
amrit180 <62633596+amrit180@users.noreply.github.com>
2020-08-15 18:55:09 +0530

    coder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,30 +6,35 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default class Contact extends Component {
     render() {
+
+
+
         return (
             <div>
-                  <div>
-                <video id="videolayout" className="video-container video-container-overlay" autoPlay="true" loop='true' muted='true'>
-                    <source src={video} type="video/mp4" />
-                    {/* <!-- 3 hi tarah ka support video type ka hota hai mp4 ogg webM --> */}
-                </video>
-            </div>
-            <div class='overlay'></div>
-            <div className="section">
-                <div class="cityText">
-                    <h1>WELCOME TO Bili</h1>
-                    <div className="contactdetails">
-                        <MdMailOutline  className="logo"/>
-                        <h1>techbili@bili.co.in</h1>
 
-<FaWhatsapp className="logo" />
-<h1>91427 60208</h1>
-                    </div>
-                    <p>You got skills? </p>
-                    <p className="pa">COME ON ! WE HAVE PROJECTS.</p>
-                   
+
+                <div>
+                    <video id="videolayout" className="video-container video-container-overlay" autoPlay="true" loop='true' muted='true'>
+                        <source src={video} type="video/mp4" />
+                        {/* <!-- 3 hi tarah ka support video type ka hota hai mp4 ogg webM --> */}
+                    </video>
                 </div>
-            </div>
+                <div class='overlay'></div>
+                <div className="section">
+                    <div class="cityText">
+                        <h1>WELCOME TO Bili</h1>
+                        <div className="contactdetails">
+                            <MdMailOutline className="logo" />
+                            <h1>techbili@bili.co.in</h1>
+
+                            <FaWhatsapp className="logo" />
+                            <h1>91427 60208</h1>
+                        </div>
+                        <p>You got skills? </p>
+                        <p className="pa">COME ON ! WE HAVE PROJECTS.</p>
+
+                    </div>
+                </div>
             </div>
         )
     }
